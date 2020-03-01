027bb44b9af027eee4622d8114050afba34b0999
leslie.alldridge <leslie.alldridge@gmail.com>
2020-03-01 17:17:01 +1300

    post and del transaction

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -4,3 +4,17 @@
 exports.getTransactions = (req, res, next) => {
   res.send("GET transactions");
 };
+
+// @desc Add transactions
+// @route POST /api/v1/transactions
+// @access Public
+exports.addTransaction = (req, res, next) => {
+  res.send("POST transactions");
+};
+
+// @desc Delete transactions
+// @route DELETE /api/v1/transactions/:id
+// @access Public
+exports.deleteTransaction = (req, res, next) => {
+  res.send("DELETE transactions");
+};
