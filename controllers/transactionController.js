28c03cd76a894282062d4e748f97b135618775ff
leslie.alldridge <leslie.alldridge@gmail.com>
2020-03-08 15:10:09 +1300

    get controller

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,20 +1,38 @@
+const Transaction = require("../models/Transaction");
+
 // @desc Get all transactions
 // @route GET /api/v1/transactions
 // @access Public
-exports.getTransactions = (req, res, next) => {
-  res.send("GET transactions");
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions
+    });
+  } catch (err) {
+    return (
+      res.send(500),
+      json({
+        success: false,
+        error: "Server Error"
+      })
+    );
+  }
 };
 
 // @desc Add transactions
 // @route POST /api/v1/transactions
 // @access Public
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
   res.send("POST transactions");
 };
 
 // @desc Delete transactions
 // @route DELETE /api/v1/transactions/:id
 // @access Public
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
   res.send("DELETE transactions");
 };
